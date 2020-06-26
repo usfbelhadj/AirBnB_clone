7035d08b2e2d4149f604a510b004b124b8438b04
usfbelhadj <usf.belhadj@gmail.com>
2020-06-26 19:27:20 +0100

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,18 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """init"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for arg in kwargs:
+                if arg == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif arg == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[arg] = kwargs[arg]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return ("{}{}{}".format(type(self).__name__, self.id, self.__dict__))
