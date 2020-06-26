833be4cfde6e64b257aca824d7701be1406b05b6
usfbelhadj <usf.belhadj@gmail.com>
2020-06-26 18:36:37 +0100

    Add Save and To_dict

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,26 @@
 
 import uuid
 from datetime import datetime
+
+
 class BaseModel:
     """Class for base model"""
-    
+
     def __init__(self, *args, **kwargs):
-        """init"""  
+        """init"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-
     def __str__(self):
-        return ("{}{}{}".format(type(self).__name__,self.id,self.__dict__))
+        return ("{}{}{}".format(type(self).__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict["class"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
