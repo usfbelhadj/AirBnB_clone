b3b080fc51f7ac05677ecde3bd62e7f47cfb53c6
achrefbs <siachrefboularess@gmail.com>
2020-06-30 10:14:51 +0000

    added comments base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -26,13 +26,16 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
+        """str"""
         return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
+        """function save"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
