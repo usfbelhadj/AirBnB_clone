97cf0deb4706b1b6015f0f29fa4d1f4fec9244ce
achrefbs <siachrefboularess@gmail.com>
2020-06-30 10:26:09 +0000

    commented console.py

--- console.py
+++ console.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
+""" entry point of the command interpreter"""
 
 
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb)'
+    """command interpreter"""
+    prompt = '(hbnb) '
 
     def default(self, line):
+        """default"""
         pass
 
     def do_quit(self, line):
-        """Exits the program.
-        """
+        """Exits the program"""
         return True
 
     def help_quit(self):
+        """help quit"""
         print("Quit command to exit the program")
 
     help_EOF = help_quit
     do_EOF = do_quit
 
     def emptyline(self):
+        """empty line"""
         pass
 
 
