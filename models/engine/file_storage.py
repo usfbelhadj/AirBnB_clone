3e710ea41d743dba344125167a9fb243a537e0f7
usfbelhadj <usf.belhadj@gmail.com>
2020-06-30 20:07:07 +0100

    Fixx PEP8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """Module for FileStorage class for the AirBnB clone console."""
+
+
 import json
 from os import path, stat
+
+
 class FileStorage:
     """Class for file Storage"""
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
+        """All"""
         return FileStorage.__objects
+
     def new(self, obj):
-        FileStorage.__objects[type(obj).__name__ +"."+obj.id] = obj
+        """New"""
+        FileStorage.__objects[type(obj).__name__ + "." + obj.id] = obj
+
     def save(self):
         '''save: to json'''
         _dict = {}
@@ -18,7 +26,7 @@ class FileStorage:
             for key, val in FileStorage.__objects.items():
                 _dict[key] = val.to_dict()
             json.dump(_dict, f)
-    
+
     def reload(self):
         from models.base_model import BaseModel
         if path.isfile(FileStorage.__file_path) and not stat(FileStorage.__file_path).st_size == 0:
